26bce130ec43562e435dd3455c2a2c3b50bdff26
Nada <nadajradbenaicha@gmail.com>
2025-07-08 12:17:20 +0200

    adding a new file and new bmi struct

---
+++ BMI Calculator/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Nada jrad on 08/07/2025.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+struct BMI {
+    let value : Float
+    let advice :String
+    let color : UIColor
+}
