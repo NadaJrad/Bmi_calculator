755c277c348ebf16076b70868f466a8cc89835f8
Nada <nadajradbenaicha@gmail.com>
2025-07-08 12:26:56 +0200

    checking the bmi value

--- BMI Calculator/CalculatorBrain.swift
+++ BMI Calculator/CalculatorBrain.swift
@@ -2,16 +2,27 @@
 import Foundation
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     func getBMIValue() -> (String) {
-        let bmiTo1DeciamlPlace = String(format: "%.1f", bmi ?? 0.0)
+        let bmiTo1DeciamlPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DeciamlPlace
         
     }
 
     mutating func calculateBMI(height: Float, weight:Float){
-         bmi = weight / (height * height)
+       let bmiValue = weight / (height * height)
+        
+        
+        if bmiValue < 18.5 {
+            
+            print("under weight")
+        } else if bmiValue < 24.5 {
+            print ("normal weight")
+        }else {
+            print ("over weight")
+        }
+//       bmi =  BMI(value: <#T##Float#>, advice: <#T##String#>, color: <#T##UIColor#>)
         
     }
 }
